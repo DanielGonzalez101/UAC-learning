b75c3c059889fde925ef1b9afa2a2b595474b6ab
DanielGonzalez101 <159564649+DanielGonzalez101@users.noreply.github.com>
2024-02-18 15:56:41 -0500

    Login terminado

--- uac-learning/src/Components/Login/Login.jsx
+++ uac-learning/src/Components/Login/Login.jsx
@@ -1,68 +1,79 @@
 import React from "react";
 import './login.css'
+// Iconos de las esquinas
+import archivo from './iconos/icono_archivo.png'
+import cuaderno from './iconos/cuaderno.png'
+import universidad from './iconos/universidad.png'
+import computador from './iconos/computador.png'
 
 const Login = () => {
-  // state and methods for handling login would go here
-  // Css
   return (
     // Contenedor de toda la pagina
-    <div className="container">
+    <>
+      <img src={archivo} id="arriba-izq" alt="" className="icon" />
+      <img src={cuaderno} id="arriba-der" alt="" className="icon" />
+      <img src={universidad} id="abajo-izq" alt="" className="icon" />
+      <img src={computador} id="abajo-der" alt="" className="icon" />
+      <div className="container">
 
-      {/* Inicia el formulario */}
-      <div id="form" >
+        {/* Inicia el formulario */}
+        <div id="form" >
 
-        {/* Titulo */}
-        <div id="title">
-          <div>
-            <h2>UAC Learning</h2>
+          {/* Titulo */}
+          <div id="title">
+            <div>
+              <h2>UAC Learning</h2>
+            </div>
           </div>
-        </div>
-        {/* Acaba titulo */}
+          {/* Acaba titulo */}
 
 
-        {/* Incian los input */}
-        <div className="input-style">
-          <input
-            type="text"
-            placeholder="Usuario"
-            id="input-usuario"
-          // onChange, value, etc.
-          />
-          <br />
-
-          <input
-            type="password"
-            placeholder="Contraseña"
-            id="input-contrasena"
-          // onChange, value, etc.
-          />
-          <br />
-          <label>
+          {/* Incian los input */}
+          <div className="input-style">
+            <p className="lbl-inputs">Usuario:</p>
             <input
-              type="checkbox"
-            // checked, onChange, etc.
-            /> recordarme
-          </label>
+              type="text"
+              placeholder="Usuario"
+              id="input-usuario"
+            // onChange, value, etc.
+            />
+            <br />
+
+            <p className="lbl-inputs">Contraseña:</p>
+            <input
+              type="password"
+              placeholder="Contraseña"
+              id="input-contrasena"
+            // onChange, value, etc.
+            />
+            <br />
+            <label>
+              <input
+                type="checkbox"
+              // checked, onChange, etc.
+              /> Recordarme
+            </label>
+          </div>
+          {/* Acaban los inputs */}
+
+
+          <br />
+          {/* Inicia recordarme */}
+          {/* Termina recordarme */}
+
+          <br />
+
+          {/* Inicia el boton */}
+          <button id="button-style" type="submit">
+            INICIAR SESIÓN
+          </button>
+          {/* termina el boto */}
+
         </div>
-        {/* Acaban los inputs */}
-
-
-        <br />
-        {/* Inicia recordarme */}
-        {/* Termina recordarme */}
-
-        <br />
-
-        {/* Inicia el boton */}
-        <button id="button-style" type="submit">
-          INICIAR SESIÓN
-        </button>
-        {/* termina el boto */}
-
+        {/* termina el form */}
       </div>
-      {/* termina el form */}
-    </div>
-    // Se acaba el contenedor de la pagina
+      {/* // Se acaba el contenedor de la pagina */}
+    </>
   );
   // Nota: En tu código origina
 }
