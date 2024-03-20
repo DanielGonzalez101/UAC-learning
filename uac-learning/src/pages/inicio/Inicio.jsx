0710720331038aac98b9d2f94d2310456261477f
DanielGonzalez101 <159564649+DanielGonzalez101@users.noreply.github.com>
2024-03-20 18:23:15 -0500

    Agrego comentarios a el inicio

--- uac-learning/src/pages/inicio/Inicio.jsx
+++ uac-learning/src/pages/inicio/Inicio.jsx
@@ -8,18 +8,25 @@ import btnEstudiante from '../../resources/inicio/icons/btn-estudiante.svg'
 const Inicio = () => {
   return (
     <>
+
       <img src={cohete} alt="" className="cohete" id="iconos" />
-      
+
       {/* Contenedor de texto */}
       <div id="contenedor-texto">
+
         <p id="texto" className="programa">
           Programa tu futuro con nosotros
         </p>
+
         <pre id="segundoParrafo">
           "La programación es como un lienzo en blanco, tú
           decides qué obra de arte crear."
         </pre>
+
       </div>
+      {/* Cierra contenedor de texto */}
+
+      {/* Abre contendor para los botones */}
       <div id="contenedor-botones">
 
         <button id="btn-profesor" className="botones">
@@ -29,7 +36,10 @@ const Inicio = () => {
         <button id="btn-estudiante" className="botones">
           <img src={btnEstudiante} alt="" id="btn-estudiante" />
         </button>
+
       </div>
+      {/* Cierra contenedor de botones */}
+
     </>
   )
 }
